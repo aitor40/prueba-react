d1733a5b2c206a16d71af689b1c13ad4f9e72eb2
aitor40 <86493991+aitor40@users.noreply.github.com>
2023-07-11 15:01:46 +0200

    Implementación de barra de búsqueda por año

--- pages/SeriesPage/SeriesPage.jsx
+++ pages/SeriesPage/SeriesPage.jsx
@@ -4,22 +4,34 @@ import { Header } from '../../src/components/Header/Header';
 import { Title } from '../../src/components/Title/Title';
 import sampleData from '../../data/sample.json';
 import './SeriesPage.css';
+import { TextField, Button, Grid } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 export const SeriesPage = () => {
 
-    const seriesData = sampleData.entries.filter( entry => (entry.programType == 'series') && ( entry.releaseYear >= 2010 ) );
+    const [year, setYear] = useState();
+    const [seriesData, setSeriesData] = useState( sampleData.entries.filter( entry => (entry.programType == 'series') && ( entry.releaseYear >= 2010 )));
+
     const seriesDataSorted = seriesData.sort((a, b) => {
         return a.title.localeCompare(b.title, 'en', { numeric: true })
     });
     const maxListSeries = seriesDataSorted.slice(0, 20);
 
-    document.addEventListener('DOMContentLoaded', () => {
-        console.log('Ha cargado!')
-    });
+    const filtraPorAno = () => {
+
+        setSeriesData( sampleData.entries.filter( entry => (entry.programType == 'series') && ( entry.releaseYear == year )));
+    
+    }
 
     return (
         <>
             <Header title="Popular Series" />
+            <Grid container className='width1200'>
+                <TextField id="outlined-basic" label="Buscar por año" variant="outlined" sx={{ mt: 2, mr: 2 }} type="number" onChange={ (e) => setYear(e.target.value) } />
+                <Button variant="contained" startIcon={ <SearchIcon /> } size="large" sx={{ mt: 2 }} onClick={ filtraPorAno }>
+                    Buscar
+                </Button>
+            </Grid>
             <main className="width1200">
             {
                     maxListSeries.map( (serie, i) => {
